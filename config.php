d698cc495cb666fc82ab31dc2da2ee89257f35e3
ernasjobs <ernasjobs@gmail.com>
2020-01-28 19:23:04 +0000

    added connection details

---
+++ config.php
@@ -0,0 +1,31 @@
+/* 
+makes a PDO connection to 'g4u' database
+throws an exception if something went wrong.
+
+anytime we want to make a connection to database we can include this script
+by making use of require('config.php') method 
+*/
+
+<?php
+$host = 'localhost';
+$db   = 'g4u';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [    
+PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,    
+PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,    
+PDO::ATTR_EMULATE_PREPARES   => false,];
+
+try 
+{    
+	$pdo = new PDO($dsn, $user, $pass, $options);
+} 
+catch (\PDOException $e) 
+{     
+	throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+?>
